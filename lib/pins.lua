5cc907455e728a3791dc3e473a1e5b77ded5a213
hotdll <hotdll@163.com>
2017-09-24 17:47:58 +0800

    更新参考手册pins模块部分

--- lib/pins.lua
+++ lib/pins.lua
@@ -10,8 +10,8 @@ local assert = base.assert
 local print = base.print
 --- 自适应GPIO模式
 -- @param pin ，参数为pio.P0_1-31 和 pio_P1_1-31 (IO >= 32 and IO - 31)
--- @number val，输出模式默认电平：0 是低电平1是高电平，中断模式0或“NEG”为下降沿，1或“POS”为上升沿。
--- @string it, 中断标志“IT”为设置当前IO为中断IO
+-- @number val，输出模式默认电平：0 是低电平1是高电平，中断模式填nil或0 or 1
+-- @string fnc, 中断按键回调函数，用来捆绑按键
 -- @return function ,返回一个函数，该函数接受一个参数用来设置IO的电平
 -- @usage key = pins.setup(pio.P1_1,0,"IT") ，配置Key的IO为pio.32,中断模式，下降沿触发。用key()获取当前电平
 -- @usage led = pins.setup(pio.P1_1,0) ,配置LED脚的IO为pio.32，输出模式，默认输出低电平。led(1)即可输出高电平
@@ -22,10 +22,8 @@ function setup(pin, val, fnc)
     -- 中断模式配置
     if type(fnc) == "function" then
         pio.pin.setdir(pio.INT, pin)
-        if (val == "POS" or 1) or (val == "NEG" or 0) then
-            --注册引脚中断的处理函数
-            rtos.on(rtos.MSG_INT, fnc)
-        end
+        --注册引脚中断的处理函数
+        rtos.on(rtos.MSG_INT, fnc)
     end
     -- 输出模式初始化默认配置
     if val ~= nil then
@@ -50,4 +48,3 @@ function setup(pin, val, fnc)
         end
     end
 end
-
