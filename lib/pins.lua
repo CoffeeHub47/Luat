38bf45fc7bc9729c2a112cae24aa1bb4dd02a3e8
hotdll <hotdll@163.com>
2017-09-25 12:03:13 +0800

    更新pins.lua的setup参数设置,更新参考手册

--- lib/pins.lua
+++ lib/pins.lua
@@ -9,21 +9,20 @@ local interruptCallbacks = {}
 
 --- 自适应GPIO模式
 -- @param pin ，参数为pio.P0_1-31 和 pio_P1_1-31 (IO >= 32 and IO - 31)
--- @number val，输出模式默认电平：0 是低电平1是高电平，中断模式填nil或0 or 1
--- @string fnc, 中断按键回调函数，用来捆绑按键
+-- @param val，输出模式默认电平：0 是低电平1是高电平，中断模式为回调函数
 -- @return function ,返回一个函数，该函数接受一个参数用来设置IO的电平
 -- @return nil,中断模式捆绑回调函数返回空
 -- @usage pins.setup(pio.P1_1,0,key_int) ，配置Key的IO为pio.32,中断模式,自动触发key_int(msg)函数
 -- @usage led = pins.setup(pio.P1_1,0) ,配置LED脚的IO为pio.32，输出模式，默认输出低电平。led(1)即可输出高电平
 -- @usage key = pins.setup(pio.P1_1),配置key的IO为pio.32，输入模式,用key()即可获得当前电平
-function setup(pin, val, fnc)
+function setup(pin, val)
     -- 关闭该IO
     pio.pin.close(pin)
     -- 中断模式配置
-    if type(fnc) == "function" then
+    if type(val) == "function" then
         pio.pin.setdir(pio.INT, pin)
         --注册引脚中断的处理函数
-        interruptCallbacks[pin] = fnc
+        interruptCallbacks[pin] = val
         return
     end
     -- 输出模式初始化默认配置
