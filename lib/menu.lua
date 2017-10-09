92cb2ffeab701b465cf7fcd808be1c1667cca44a
hotdll <hotdll@163.com>
2017-10-09 18:08:42 +0800

    增加图标

--- lib/menu.lua
+++ lib/menu.lua
@@ -6,20 +6,28 @@
 -- @release 2017.09.30 16:00
 module(..., package.seeall)
 
+--LCD分辨率的宽度和高度(单位是像素)
+WIDTH,HEIGHT,BPP = disp.getlcdinfo()
+--1个ASCII字符宽度为8像素，高度为16像素；汉字宽度和高度都为16像素
+CHAR_WIDTH = 8
 -- 同屏菜单级数最大值
-local SHOW_MAX = 8
+local SHOW_MAX = 4
 -- 同级菜单级数最大值
 local ITEMS_MAX = 8
 
 -- 菜单表单
 local itemTab = {
     itemId = 1, -- 菜单当前节点
-    title = "", -- 菜单标题
-    submenu = {} -- 子菜单 or 子条目
-    supmenu = {} -- 父菜单 or 父条目
-    subFnc = nil -- 节点函数
+    subFnc = nil, -- 节点函数
+    title = {}, -- 同级菜单标题
+    supmenu = {}, -- 父菜单 or 父条目
+    submenu = {}, -- 子菜单 or 子条目
 }
 
-function append(title,supid,fnc)
+function append(title, supid, fnc)
+
+end
+
+function displayMenu()
 
 end
--- testLcd.lua
+++ testLcd.lua
@@ -5,11 +5,18 @@ module(..., package.seeall)
 --清空LCD显示缓冲区
 disp.clear()
 --从坐标16,0位置开始显示"欢迎使用Luat"
-disp.puttext("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ中", 0, 0)
--- disp.puttext("abcdefghijklmnop", 0, 16)
--- disp.puttext("ABCDEFGHIJKLMNOP", 0, 32)
--- disp.puttext("~!@#$%^&*()_+{}|", 0, 48)
+-- disp.puttext("-- 语言菜单 --", 0, 0)
+-- disp.puttext("   英文菜单", 0, 16)
+-- disp.puttext(" > 德文菜单", 0, 32)
+-- disp.puttext("   日文菜单", 0, 48)
 --显示logo图片
---disp.putimage("/ldata/logo_"..(lcd.BPP==1 and "mono.bmp" or "color.png"),lcd.BPP==1 and 41 or 1,lcd.BPP==1 and 18 or 33)
+-- disp.setcolor(0x0000)
+-- disp.drawrect(0,32,64,32,0xffff)
+-- disp.setcolor(0xffff)
+disp.putimage("/ldata/1.bmp",0,0,-1)
+disp.putimage("/ldata/2.bmp",64,0,-1)
+disp.putimage("/ldata/up.bmp",56,16)
+-- disp.puttext("▼", 60, 32)
+-- disp.puttext("V", 60, 40)
 --刷新LCD显示缓冲区到LCD屏幕上
 disp.update()
