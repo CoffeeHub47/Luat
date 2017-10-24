50fd44efacf48dde4a72dffc5a7520833bdde382
hotdll <hotdll@163.com>
2017-10-24 16:54:56 +0800

    新增lua工具包库 utils.lua

--- lib/http.lua
+++ lib/http.lua
@@ -4,31 +4,70 @@
 -- @lincense MIT
 -- @copyright OpenLuat.com
 -- @release 2017.10.23
-require("socket")
+require "socket"
+require "utils"
 module(..., package.seall)
--- 错误消息预定义
-local SOCKET_SSL_ERROR = 0
-local SOCKET_ERROR = 1
-
+--[[错误消息预定义
+"SOCKET_SSL_ERROR"
+"SOCKET_CONN_ERROR"
+"SOCKET_SEND_ERROR"
+SOCKET_RECV_TIMOUT
+--]]
+local message = {
+    "GET ",
+    "head",
+    " HTTP/1.1\n",
+    "Accept: */*\n",
+    "Accept-Language: zh-CN,zh,cn\n",
+    "User-Agent: Mozilla/4.0\n",
+    "Host: ",
+    "wthrcdn.etouch.cn",
+    "\n\n\n",
+    "Connection: Keep-Alive\n",
+    "Content-Type: application/x-www-form-urlencoded\n",
+    "Content-Length:",
+    "0",
+    "\nConnection:close\n",
+    ""
+}
 --- 创建HTTP客户端
--- @param c,socket 对象实例
--- @return table,返回一个http客户端的实例
-function request(type, url)
-    local ssl = string.find(rtos.get_version(), "SSL")
-    local https = string.find(url, "https://")
-    if ssl == nil and https then return SOCKET_SSL_ERROR end
-    local index = string.find(url, "://")
-    if index then index = index + 3 else index = 1 end
-    -- 查找"http://"后到第一个"/"之间存在@符号没有
-    local auth = string.find(url, "@")
-    local sub = string.find(url, "/", index)
-    -- 如果找到@在"/"之前，将@之前的字符串赋值给usr
-    if auth and sub > auth then
-        local usr = string.sub(url, index, auth)
-        index = auth + 1
+-- @string put,提交方式"GET" or "POST"
+-- @string url,HTTP请求超链接
+-- @string data,"POST"提交的数据表
+-- @return string ,HttpServer返回的数据
+function request(put, url, data)
+    -- 数据，端口,主机,
+    local msg, port, host, len, sub, head, str = {}
+    -- 判断SSL支持是否满足
+    local ssl, https = rtos.get_version():find("SSL"), url:find("https://")
+    if ssl == nil and https then return "SOCKET_SSL_ERROR" end
+    -- 对host:port整形
+    if url:find("://") then url = url:sub(8) end
+    sub = url:find("/")
+    if not sub then url = url .. "/"; sub = -1 end
+    str = url:match("([%w%.%-%:]+)/")
+    port = str:match(":(%d+)") or 80
+    host = str:match("[%w%.%-]+")
+    head = url:sub(sub)
+    for k, v in pairs(data) do
+        table.insert(msg, k .. "=" .. v)
+        table.insert(msg, "&")
     end
-    local host = string.
-        local c = socket.tcp()
-    if not c:connect(host, port) then return SOCKET_ERROR end
-
+    table.remove(msg)
+    str = table.concat(msg)
+    len = str:utf8len()
+    str = string.urlencoded(str)
+    message[1] = put
+    message[2] = head
+    message[8] = host
+    message[13] = len
+    message[15] = str
+    str = table.concat(message)
+    local c = socket.tcp()
+    if not c:connect(host, port) then c:close() return "SOCKET_CONN_ERROR" end
+    if not c:send(str) then c:close() return "SOCKET_SEND_ERROR" end
+    local r, s = c:recv()
+    c:close()
+    if not r then return "SOCKET_RECV_TIMOUT" end
+    return s
 end
--- lib/utils.lua
+++ lib/utils.lua
@@ -4,7 +4,6 @@
 -- @license MIT
 -- @copyright openLuat.com
 -- @release 2017.10.19
-
 module(..., package.seeall)
 
 --- 返回字符串的16进制表示
@@ -18,4 +17,138 @@ function hexlify(str)
     return str:gsub('.', function(c)
         return string.format("%02X", string.byte(c))
     end)
+end
+
+--- 返回utf8编码字符串的长度
+-- @string str,utf8编码的字符串,支持中文
+-- @return number,返回字符串长度
+-- @usage local cnt = string.utf8len("中国"),str = 2
+function string.utf8len(str)
+    local len = #str
+    local left = len
+    local cnt = 0
+    local arr = {0, 0xc0, 0xe0, 0xf0, 0xf8, 0xfc}
+    while left ~= 0 do
+        local tmp = string.byte(str, -left)
+        local i = #arr
+        while arr[i] do
+            if tmp >= arr[i] then
+                left = left - i
+                break
+            end
+            i = i - 1
+        end
+        cnt = cnt + 1
+    end
+    return cnt
+end
+-- 将一个字符转为urlencode编码
+local function urlencodeChar(c)
+    return "%" .. string.format("%02X", string.byte(c))
+end
+--- 返回字符串的urlencode编码
+-- @string str，要转换编码的字符串
+-- @return str,urlencode编码的字符串
+-- @usage string.urlencode("####133")
+function string.urlencode(str)
+    return string.gsub(string.gsub(string.gsub(tostring(str), "\n", "\r\n"), "([^%w%.%- ])", urlencodeChar), " ", "+")
+end
+--- 返回数字的千位符号格式
+-- @number num,数字
+-- @return string，千位符号的数字字符串
+-- @usage loca s = string.formatNumberThousands(1000) ,s = "1,000"
+function string.formatNumberThousands(num)
+    local k, formatted
+    formatted = tostring(tonumber(num))
+    while true do
+        formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+        if k == 0 then break end
+    end
+    return formatted
+end
+--- 判断文件是否存在
+-- @string path,文件全名例如："/ldata/call.mp3"
+-- @return boole,存在为true,不存在为false
+-- @usage local ex = io.exists("/ldata/call.mp3")
+function io.exists(path)
+    local file = io.open(path, "r")
+    if file then
+        io.close(file)
+        return true
+    end
+    return false
+end
+--- 读取文件并返回文件的内容
+-- @string path,文件全名例如："/ldata/call.txt"
+-- @return string,文件的内容,文件不存在返回nil
+-- @usage local c = io.readfile("/ldata/call.txt")
+function io.readfile(path)
+    local file = io.open(path, "r")
+    if file then
+        local content = file:read("*a")
+        io.close(file)
+        return content
+    end
+    return nil
+end
+--- 写入文件指定的内容,默认为覆盖二进制模式
+-- @string path,文件全名例如："/ldata/call.txt"
+-- @string content,文件内容
+-- @string mode,文件写入模式默认"w+b"
+-- @return string,文件的内容
+-- @usage local c = io.writefile("/ldata/call.txt","test")
+function io.writefile(path, content, mode)
+    mode = mode or "w+b"
+    local file = io.open(path, mode)
+    if file then
+        if file:write(content) == nil then return false end
+        io.close(file)
+        return true
+    else
+        return false
+    end
+end
+--- 将文件路径分解为table信息
+-- @string path,文件路径全名例如:"/ldata/call.txt"
+-- @return table,{dirname="/ldata/",filename="call.txt",basename="call",extname=".txt"}
+-- @usage loca p = io.pathinfo("/ldata/call.txt")
+function io.pathinfo(path)
+    local pos = string.len(path)
+    local extpos = pos + 1
+    while pos > 0 do
+        local b = string.byte(path, pos)
+        if b == 46 then -- 46 = char "."
+            extpos = pos
+        elseif b == 47 then -- 47 = char "/"
+            break
+        end
+        pos = pos - 1
+    end
+    
+    local dirname = string.sub(path, 1, pos)
+    local filename = string.sub(path, pos + 1)
+    extpos = extpos - pos
+    local basename = string.sub(filename, 1, extpos - 1)
+    local extname = string.sub(filename, extpos)
+    return {
+        dirname = dirname,
+        filename = filename,
+        basename = basename,
+        extname = extname
+    }
+end
+--- 返回文件大小
+-- @string path,文件路径全名例如:"/ldata/call.txt","test"
+-- @return number ,文件大小
+-- @usage locan cnt = io.filesize("/ldata/call.txt")
+function io.filesize(path)
+    local size = 0
+    local file = io.open(path, "r")
+    if file then
+        local current = file:seek()
+        size = file:seek("end")
+        file:seek("set", current)
+        io.close(file)
+    end
+    return size
 end
