43266a171aa06e7914fcf5f67181c9dd7027a2c1
hotdll <hotdll@163.com>
2017-10-26 17:38:26 +0800

    测试获取和写入星历

--- lib/agps.lua
+++ lib/agps.lua
@@ -4,5 +4,18 @@
 -- @license MIT
 -- @copyright OpenLuat.com
 -- @release 2017.10.23
-require "socket"
+require "http"
 module(..., package.seeall)
+
+local AlmanacData = "/AlmanacData.dat"
+-- 测试HTTP任务
+function setup(timeout, upTime)
+    sys.taskInit(function(timeout, upTime)
+        while true do
+            while not socket.isReady() do sys.wait(1000) end
+            local code, head, data = http.request("GET", "download.openluat.com/9501-xingli/brdcGPD.dat_rda", timeout)
+            if code == "200" then io.writefile(AlmanacData, common.binstohexs(data), "w") end
+            sys.wait(upTime)
+        end
+    end, timeout, upTime)
+end
