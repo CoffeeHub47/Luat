d0b04045ef7a88bcb9e9a99d7bdf56103aa4983b
hotdll <hotdll@163.com>
2017-11-20 09:38:38 +0800

    测试‘维非’提供的不正常的链接

--- demo/testMqtt.lua
+++ demo/testMqtt.lua
@@ -1,22 +1,22 @@
 --- testMqtt
 -- @module testMqtt
--- @author 小强
+-- @author ??
 -- @license MIT
 -- @copyright openLuat.com
 -- @release 2017.10.24
-
 require "mqtt"
 
 sys.taskInit(function()
     while not socket.isReady() do sys.wait(10000) end
     local mqttc = mqtt.client(misc.getimei())
     while true do
-        while not mqttc:connect("180.97.80.55", 1883) do
+        -- while not mqttc:connect("180.97.80.55", 1883) do
+        while not mqttc:connect("1.80.251.253", 1883) do
             sys.wait(2000)
         end
-        if mqttc:subscribe(string.format("/device/%s/req", misc.getimei())) then
+        if mqttc:subscribe(string.format("/device/%s/req", --[[misc.getimei()]] "868575021629300")) then
             local count = 0
-
+            
             if mqttc:publish(string.format("/device/%s/report", misc.getimei()), "test publish " .. count) then
                 while true do
                     local r, data = mqttc:receive(2000)
